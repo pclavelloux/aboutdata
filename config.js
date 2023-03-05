f3eaeddd5bc5b098f5173a94a8f470fe2bf95a13
paulineclavelloux <pauline.clavelloux@gmail.com>
2023-03-05 21:29:26 +0100

    cloudbuild yaml et dotenv

---
+++ config.js
@@ -0,0 +1,4 @@
+// Import all env vars from .env file
+require('dotenv').config()
+export const _NEXT_PUBLIC_SUPABASE_URL = process.env._NEXT_PUBLIC_SUPABASE_URL
+export const _NEXT_PUBLIC_SUPABASE_ANON_KEY = process.env._NEXT_PUBLIC_SUPABASE_ANON_KEY
