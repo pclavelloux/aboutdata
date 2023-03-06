2b4140c3a0a60bb69ea366c00ee6bd744ef7d79d
paulineclavelloux <pauline.clavelloux@gmail.com>
2023-03-06 17:15:56 +0100

    clean

--- config.js
+++
@@ -1,4 +0,0 @@
-// Import all env vars from .env file
-require('dotenv').config()
-export const NEXT_PUBLIC_SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL
-export const NEXT_PUBLIC_SUPABASE_ANON_KEY = process.env.NEXTPUBLIC_SUPABASE_ANON_KEY
